f0b984f5983d12fcc796193da377d69b6b84fe3a
ArinaUrnysheva <arinaurnysheva@yandex.ru>
2024-05-02 09:35:34 +0300

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    cout<<endl;
+    for(int i = 0; i<n; ++i){
+        cout<<"I will never loose deadline!"<<endl;
+    }
+    return 0;
+}
